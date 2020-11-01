70a8594f04d902b6da7b0093be5ad48ce94c4e7d
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-11-01 14:57:17 -0500

    refactored code

--- mapViewer.js
+++ mapViewer.js
@@ -1,14 +1,20 @@
 angular.module('open-civ')
-.directive('mapViewer', function() {
+.directive('mapViewer', function(terrain) {
+    console.log({terrain})
     return {
         scope: {
             rows: '&',
+        },
+        link: function (scope, element, attrs) {
+            scope.getIndex = function () {
+                return 1;
+            }
         },
         template: `
         <div ng-repeat="row in rows()"
             style="line-height: 0px;">
             <div ng-repeat="col in row track by $index"
-                style="display:inline;"><img src="images/terrain/{{col}}1.png"
+                style="display:inline;"><img src="images/terrain/{{col}}{{getIndex(col)}}.png"
                     style="display: inline"/></div>
         </div>
         `
