20c2ead96d1ca997058e2740022568c0818bc622
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-11-01 17:28:35 -0500

    Update mapEditor.js

--- mapEditor.js
+++ mapEditor.js
@@ -4,74 +4,91 @@ angular.module('open-civ')
         scope: {
         },
         link: function(scope, element, attrs) {
-            let rowCount = 50;
-            let colCount = 100;
-
-            scope.map = [];
-            for (let i = 0; i < rowCount; i++) {
-                scope.map.push([]);
-                for (let j = 0; j < colCount; j++) {
-                    scope.map[i].push('ocean');
-                }
-            }
+            let rowCount = 60;
+            let colCount = 2*rowCount;
 
             let mid = Math.floor(rowCount / 2);
 
-            scope.iterate1 = function () {
+            let continentCount = 7;
+
+            scope.seed = function () {
+                scope.map = [];
+                for (let i = 0; i < rowCount; i++) {
+                    scope.map.push([]);
+                    for (let j = 0; j < colCount; j++) {
+                        scope.map[i].push('ocean');
+                    }
+                }
+
                 Math.seedrandom(new Date().toString());
                 for (let i = 0; i < rowCount; i++) {
                     for (let j = 0; j < colCount; j++) {
-                        if (scope.map[i][j] === 'ocean') {
-                            if (randomIntFromInterval(0, distanceFromEquator(mid, i) / 8) === 0) {
-                                scope.map[i][j] = 'coast';
-                            }
+                        if (Math.random() < .5) {
+                            scope.map[i][j] = 'mountains';
+                        }
+                        if (Math.random() < .05) {
+                            scope.map[i][j] = 'volcano';
                         }
                     }
                 }
             }
 
-            scope.iterate2 = function () {
+            let water = ['ocean', 'coast'];
+            
+            scope.iterate = function () {
                 for (let i = 0; i < rowCount; i++) {
                     for (let j = 0; j < colCount; j++) {
-                        if (scope.map[i][j] === 'coast') {
-                            let n = neighbors(scope.map, i, j, 4);
-                            // 50% neighbors ocean
-                            if (n.filter(o => o === 'ocean').length/n.length > .8) {
+                        let n1 = neighbors(scope.map, i, j, 1);
+                        let n2 = neighbors(scope.map, i, j, 2);
+                        if (scope.map[i][j] === 'ocean') {
+                            // If we have a non-water neighbor, make us coast
+                            if (n1.filter(n => !water.includes(n)).length > 0) {
+                                scope.map[i][j] = 'coast';
+                            }
+                            if (n1.filter(n => n !== 'mountains').length === 0) {
+                                scope.map[i][j] = 'mountains';
+                            }
+                        } else if (scope.map[i][j] === 'coast') {
+                            // All neighbors within distance 2 are water
+                            if (n2.filter(n => water.includes(n)).length === n2.length) {
                                 scope.map[i][j] = 'ocean';
+                                console.log('here')
+                            }
+
+                            // No ocean, all land or coast
+                            if (n2.filter(n => n === 'ocean').length === 0) {
+                                scope.map[i][j] = randomLand(i, mid);
+                            }
+                        } else if (scope.map[i][j] === 'mountains') {
+                            // If we have a neighbor other than 
+                            if (n1.filter(n => water.includes(n)).length > 2) {
+                                scope.map[i][j] = 'coast';
+                            }
+                        } else if (scope.map[i][j] === 'volcano') {
+                            if (n1.filter(n => water.includes(n)).length > 0) {
+                                scope.map[i][j] = 'mountains';
                             }
                         }
                     }
                 }
             }
 
-            // scope.iterate2 = function () {
-            //     Math.seedrandom(new Date().toString());
-            //     for (let i = 0; i < rowCount; i++) {
-            //         for (let j = 0; j < colCount; j++) {
-            //             if (scope.map[i][j] === 'ocean') {
-            //                 let n = neighbors(scope.map, i, j);
-            //                 console.log(n);
-            //                 if (n.filter(o => o === 'coast').length === 4) {
-            //                     scope.map[i][j] = 'coast'
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }
-            scope.iterate1();
-            
-            scope.iterate2();
-            scope.iterate2();
-            scope.iterate2();
-            scope.iterate2();
+            scope.seed();
+            scope.iterate();
+            scope.iterate();
         },
         template: `
-        <button ng-click="iterate1()">Iterate1</button>
-        <button ng-click="iterate2()">Iterate2</button>
+        <button ng-click="seed()">seed</button>
+        <button ng-click="iterate()">iterate</button>
         <map-viewer rows="map"></map-viewer>
         `
     }
 });
+
+function randomLand(row, mid) {
+    // TODO:
+    return 'grass';
+}
 
 function distanceFromEquator(mid, row) {
     if (row > mid) {
