319ad4cd7a97ee21e1f906b74bb5e5cf4f13394b
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-11-01 19:14:38 -0500

    refactored code

--- mapEditor.js
+++ mapEditor.js
@@ -1,193 +1,197 @@
 angular.module('open-civ')
-.directive('mapEditor', function(randomIntFromInterval) {
+.directive('mapEditor', function() {
     return {
         scope: {
         },
         link: function(scope, element, attrs) {
-            let rowCount = 50;
-            let colCount = 2*rowCount;
-
-            let mid = Math.floor(rowCount / 2);
-
-            let continentCount = 7;
-
-            scope.seed = function () {
-                Math.seedrandom(new Date().toString());
-
-                scope.map = [];
-                for (let i = 0; i < rowCount; i++) {
-                    scope.map.push([]);
-                    for (let j = 0; j < colCount; j++) {
-                        scope.map[i].push('ocean');
-                    }
-                }
-
-                for (let i = 0; i < rowCount; i++) {
-                    for (let j = 0; j < colCount; j++) {
-                        if (Math.random() < .485) {
-                            scope.map[i][j] = 'mountains';
-                        }
-                        if (Math.random() < .05) {
-                            scope.map[i][j] = 'volcano';
-                        }
-                    }
-                }
-            }
-
-            let water = ['ocean', 'coast'];
-            
-            scope.iterate = function () {
-                for (let i = 0; i < rowCount; i++) {
-                    for (let j = 0; j < colCount; j++) {
-                        let n1 = neighbors(scope.map, i, j, 1);
-                        let n2 = neighbors(scope.map, i, j, 2);
-                        let n3 = neighbors(scope.map, i, j, 3);
-                        if (scope.map[i][j] === 'ocean') {
-                            // If we have a non-water neighbor, make us coast
-                            if (n1.filter(n => !water.includes(n)).length > 0) {
-                                scope.map[i][j] = 'coast';
-                            }
-                            if (n1.filter(n => n !== 'mountains').length === 0) {
-                                scope.map[i][j] = 'mountains';
-                            }
-                        } else if (scope.map[i][j] === 'coast') {
-                            // All neighbors within distance 2 are water
-                            if (n2.filter(n => water.includes(n)).length === n2.length) {
-                                scope.map[i][j] = 'ocean';
-                            }
-
-                            // No ocean, all land or coast
-                            if (n2.filter(n => n === 'ocean').length === 0) {
-                                scope.map[i][j] = 'grass';
-                            }
-                        } else if (scope.map[i][j] === 'mountains') {
-                            if (n2.filter(n => n === 'grass').length / n2.length > .35) {
-                                scope.map[i][j] = 'grass';
-                            }
-                            // Over half neighbors water makes mountain coast.
-                            if (n1.filter(n => water.includes(n)).length / n1.length > .5) {
-                                scope.map[i][j] = 'coast';
-                            }
-                        } else if (scope.map[i][j] === 'volcano') {
-                            // Volcano next to water becomes mountains
-                            if (n1.filter(n => water.includes(n)).length > 0) {
-                                scope.map[i][j] = 'mountains';
-                            }
-                        } else if (scope.map[i][j] === 'grass') {
-                            if (n3.filter(n => water.includes(n)).length / n3.length < .005) {
-                                scope.map[i][j] = 'plains';
-                            }
-                        }
-                    }
-                }
-            }
-
-            scope.addIce = function () {
-                for (let i = 0; i < rowCount; i++) {
-                    for (let j = 0; j < colCount; j++) {
-                        if (['grass', 'plains'].includes(scope.map[i][j])) {
-                            let d = distanceFromEquator(mid, i) / mid;
-                            if (d > .7) {
-                                if (Math.random() > .7) {
-                                    scope.map[i][j] = 'ice';
-                                }
-                            }
-                            if (d > .8) {
-                                if (Math.random() > .3) {
-                                    scope.map[i][j] = 'ice';
-                                }
-                            }
-                            if (d > .9) {
-                                if (Math.random() > .05) {
-                                    scope.map[i][j] = 'ice';
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-
-            scope.addPlains = function () {
-                for (let i = 0; i < rowCount; i++) {
-                    for (let j = 0; j < colCount; j++) {
-                        if (['grass'].includes(scope.map[i][j])) {
-                            let d = distanceFromEquator(mid, i) / mid;
-                            if (d < .35) {
-                                if (Math.random() > .5) {
-                                    scope.map[i][j] = 'plains';
-                                }
-                            }
-                            if (d < .2) {
-                                if (Math.random() > .1) {
-                                    scope.map[i][j] = 'plains';
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-
-            scope.addDesert = function () {
-                for (let i = 0; i < rowCount; i++) {
-                    for (let j = 0; j < colCount; j++) {
-                        if (['plains'].includes(scope.map[i][j])) {
-                            let d = distanceFromEquator(mid, i) / mid;
-                            if (d < .25) {
-                                if (Math.random() > .8) {
-                                    scope.map[i][j] = 'desert';
-                                }
-                            }
-                            if (d < .1) {
-                                if (Math.random() > .2) {
-                                    scope.map[i][j] = 'desert';
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-
-            scope.addJungle = function () {
-                for (let i = 0; i < rowCount; i++) {
-                    for (let j = 0; j < colCount; j++) {
-                        if (['grass'].includes(scope.map[i][j])) {
-                            let d = distanceFromEquator(mid, i) / mid;
-                            let n3 = neighbors(scope.map, i, j, 3);
-                            if (n3.filter(n => water.includes(n)).length / n3.length > .15) {
-                                if (Math.random() > .5) {
-                                    if (d < .3) {
-                                        scope.map[i][j] = 'jungle';
-                                    } else {
-                                        scope.map[i][j] = 'forest';
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-
-            scope.seed();
-
-            scope.iterate();
-            scope.iterate();
-            scope.iterate();
-
-            scope.addIce();
-
-            scope.addPlains();
-
-            scope.addDesert();
-
-            scope.addJungle();
+            scope.map = generateMap(100, 200);
         },
         template: `
-        <button ng-click="seed()">seed</button>
-        <button ng-click="iterate()">iterate</button>
         <map-viewer rows="map"></map-viewer>
         `
     }
 });
+
+function generateMap() {
+    let map;
+
+    let rowCount = 50;
+    let colCount = 2*rowCount;
+
+    let mid = Math.floor(rowCount / 2);
+
+    let seed = function () {
+        Math.seedrandom(new Date().toString());
+
+        map = [];
+        for (let i = 0; i < rowCount; i++) {
+            map.push([]);
+            for (let j = 0; j < colCount; j++) {
+                map[i].push('ocean');
+            }
+        }
+
+        for (let i = 0; i < rowCount; i++) {
+            for (let j = 0; j < colCount; j++) {
+                if (Math.random() < .485) {
+                    map[i][j] = 'mountains';
+                }
+                if (Math.random() < .05) {
+                    map[i][j] = 'volcano';
+                }
+            }
+        }
+    }
+
+    let water = ['ocean', 'coast'];
+    
+    let iterate = function () {
+        for (let i = 0; i < rowCount; i++) {
+            for (let j = 0; j < colCount; j++) {
+                let n1 = neighbors(map, i, j, 1);
+                let n2 = neighbors(map, i, j, 2);
+                let n3 = neighbors(map, i, j, 3);
+                if (map[i][j] === 'ocean') {
+                    // If we have a non-water neighbor, make us coast
+                    if (n1.filter(n => !water.includes(n)).length > 0) {
+                        map[i][j] = 'coast';
+                    }
+                    if (n1.filter(n => n !== 'mountains').length === 0) {
+                        map[i][j] = 'mountains';
+                    }
+                } else if (map[i][j] === 'coast') {
+                    // All neighbors within distance 2 are water
+                    if (n2.filter(n => water.includes(n)).length === n2.length) {
+                        map[i][j] = 'ocean';
+                    }
+
+                    // No ocean, all land or coast
+                    if (n2.filter(n => n === 'ocean').length === 0) {
+                        map[i][j] = 'grass';
+                    }
+                } else if (map[i][j] === 'mountains') {
+                    if (n2.filter(n => n === 'grass').length / n2.length > .35) {
+                        map[i][j] = 'grass';
+                    }
+                    // Over half neighbors water makes mountain coast.
+                    if (n1.filter(n => water.includes(n)).length / n1.length > .5) {
+                        map[i][j] = 'coast';
+                    }
+                } else if (map[i][j] === 'volcano') {
+                    // Volcano next to water becomes mountains
+                    if (n1.filter(n => water.includes(n)).length > 0) {
+                        map[i][j] = 'mountains';
+                    }
+                } else if (map[i][j] === 'grass') {
+                    if (n3.filter(n => water.includes(n)).length / n3.length < .005) {
+                        map[i][j] = 'plains';
+                    }
+                }
+            }
+        }
+    }
+
+    let addIce = function () {
+        for (let i = 0; i < rowCount; i++) {
+            for (let j = 0; j < colCount; j++) {
+                if (['grass', 'plains'].includes(map[i][j])) {
+                    let d = distanceFromEquator(mid, i) / mid;
+                    if (d > .7) {
+                        if (Math.random() > .7) {
+                            map[i][j] = 'ice';
+                        }
+                    }
+                    if (d > .8) {
+                        if (Math.random() > .3) {
+                            map[i][j] = 'ice';
+                        }
+                    }
+                    if (d > .9) {
+                        if (Math.random() > .05) {
+                            map[i][j] = 'ice';
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    let addPlains = function () {
+        for (let i = 0; i < rowCount; i++) {
+            for (let j = 0; j < colCount; j++) {
+                if (['grass'].includes(map[i][j])) {
+                    let d = distanceFromEquator(mid, i) / mid;
+                    if (d < .35) {
+                        if (Math.random() > .5) {
+                            map[i][j] = 'plains';
+                        }
+                    }
+                    if (d < .2) {
+                        if (Math.random() > .1) {
+                            map[i][j] = 'plains';
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    let addDesert = function () {
+        for (let i = 0; i < rowCount; i++) {
+            for (let j = 0; j < colCount; j++) {
+                if (['plains'].includes(map[i][j])) {
+                    let d = distanceFromEquator(mid, i) / mid;
+                    if (d < .25) {
+                        if (Math.random() > .8) {
+                            map[i][j] = 'desert';
+                        }
+                    }
+                    if (d < .1) {
+                        if (Math.random() > .2) {
+                            map[i][j] = 'desert';
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    let addJungle = function () {
+        for (let i = 0; i < rowCount; i++) {
+            for (let j = 0; j < colCount; j++) {
+                if (['grass'].includes(map[i][j])) {
+                    let d = distanceFromEquator(mid, i) / mid;
+                    let n3 = neighbors(map, i, j, 3);
+                    if (n3.filter(n => water.includes(n)).length / n3.length > .15) {
+                        if (Math.random() > .5) {
+                            if (d < .3) {
+                                map[i][j] = 'jungle';
+                            } else {
+                                map[i][j] = 'forest';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    seed();
+
+    iterate();
+    iterate();
+    iterate();
+
+    addIce();
+
+    addPlains();
+
+    addDesert();
+
+    addJungle();
+
+    return map;
+}
 
 function distanceFromEquator(mid, row) {
     if (row > mid) {
